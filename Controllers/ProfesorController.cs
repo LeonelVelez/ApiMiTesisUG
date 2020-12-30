1358fc52a1da0549a94786669e466142c25d7a5d
Leonel Velez <leonel-14-velez@hotmail.com>
2020-12-30 15:09:52 -0500

    agregando controlador profesor

---
+++ Controllers/ProfesorController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiMiTesisUG.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiMiTesisUG.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("[controller]")]
+    public class ProfesorController : ControllerBase
+    {        
+
+        private readonly ILogger<ProfesorController> _logger;
+
+        public ProfesorController(ILogger<ProfesorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [Route("obtener")]
+        public ActionResult<Profesor> Obtener(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [Route("obtener/{idcarrera}")]
+        public ActionResult<Profesor> ObtenerCarrera(int idcarrera)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [Route("obtener/{idestado}")]
+        public ActionResult<Profesor> ObtenerEstadoTesis(int idestado)
+        {
+            return Ok();
+        }
+        
+    }
+}
